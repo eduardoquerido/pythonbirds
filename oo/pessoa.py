4fe292bcf50dbff7b98722f65e46c0a76a5d7b19
Eduardo Querido <eduardo.rumbels@gmail.com>
2019-09-09 22:14:34 -0300

    Criado atributo complexo 'filhos'

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,15 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=28):
+    def __init__(self, *filhos, nome=None, idade=28):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Querido')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Eduardo'
-    print(p.nome)
+    rumbels = Pessoa(nome='Rumbelsperger')
+    querido = Pessoa(rumbels, nome='Querido')
+    print(Pessoa.cumprimentar(querido))
+    print(id(querido))
+    print(querido.cumprimentar())
+    print(querido.nome)
+    print(querido.idade)
+    for filho in querido.filhos:
+        print(filho.nome)
