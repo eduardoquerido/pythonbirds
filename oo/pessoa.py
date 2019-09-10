8e21c08a93065df835779fb35c41f67d11f30281
Eduardo Querido <eduardo.rumbels@gmail.com>
2019-09-09 22:24:46 -0300

    Criado alguns comentários

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,15 +1,15 @@
 class Pessoa:
-    def __init__(self, *filhos, nome=None, idade=28):
-        self.idade = idade
+    def __init__(self, *filhos, nome=None, idade=28): #tudo dentro dos parenteses são os parâmetros
+        self.idade = idade #self.idade é um atributo
         self.nome = nome
         self.filhos = list(filhos)
 
-    def cumprimentar(self):
+    def cumprimentar(self): #método é diferente de uma função pois está dentro de uma classe e sempre está atrelado a um objeto
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    rumbels = Pessoa(nome='Rumbelsperger')
-    querido = Pessoa(rumbels, nome='Querido')
+    rumbelsperger = Pessoa(nome='Rumbelsperger')
+    querido = Pessoa(rumbelsperger, nome='Querido') #atributo complexo, rumbelsperger depende do querido
     print(Pessoa.cumprimentar(querido))
     print(id(querido))
     print(querido.cumprimentar())
@@ -17,3 +17,5 @@ if __name__ == '__main__':
     print(querido.idade)
     for filho in querido.filhos:
         print(filho.nome)
+    querido.sobrenome = 'Ferreira' #atributo dinâmico
+    print(querido.sobrenome)
